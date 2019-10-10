b9328d8c4f55d152a983bc3c6bb17424639c17be
Alexandr <boralexm90@gmail.com>
2019-10-11 00:37:27 +0300

    implement HW_1

---
+++ Lesson_1/Variable.java
@@ -0,0 +1,12 @@
+public class Variable {
+	public static void main (String[] args ) {
+		byte typeOne = -56;
+		short typeTwo = -11549;
+		int typeThree = 5;
+		long typeFour = 3123857;
+		float typeFive = 1853,18f;
+		double typeSix = 9,80665d;
+		char typeSeven = 'I';
+		boolean typeEight = true;
+	}
+}
