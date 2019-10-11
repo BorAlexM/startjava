499de886fca286f0405f59a17ba6b1cab34b4bd0
Alexandr <boralexm90@gmail.com>
2019-10-11 22:03:10 +0300

    corrected HW_1, HW_2, implement HW_3

--- Lesson_1/ConditionalStatement.java
+++ Lesson_1/ConditionalStatement.java
@@ -1,25 +1,24 @@
 public class ConditionalStatement {
-	public static void main(String[] args){
+	public static void main(String[] args) {
 		int yourAge = 25;
-		if (yourAge > 20){
-			System.out.println ("Вы закончил школу");
+		if(yourAge > 20) {
+			System.out.println ("Вы закончили школу");
 		}
-		boolean yourMan = true;
-		if (yourMan == true){
-			System.out.println ("Вы не женщина");
+
+		boolean isMan = true;
+		if(isMan) {
+			System.out.println ("Вы мужчина");
 		}
-		boolean yourNotMan = true;
-		if (yourNotMan == true){
-			System.out.println ("Вы женщина");
-		}
+
 		float yourHeight = 1.50f;
-		if (yourHeight < 1.80){
+		if(yourHeight < 1.80) {
 			System.out.println ("Вы среднестатистический");
 		}	else {
 			System.out.println ("Вы высокий");
 		}
+
 		char firstLetterYourName = 'A';
-		if (firstLetterYourName == 'M'){
+		if(firstLetterYourName == 'M') {
 			System.out.println ("Вас зовут Michael");
 		}	else if (firstLetterYourName == 'I') {
 			System.out.println ("Вас зовут Ignat");
