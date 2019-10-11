95518c776dea2cddcb95a8c2fa3384507a1d1b0d
Alexandr <boralexm90@gmail.com>
2019-10-11 21:58:13 +0300

    corrected HW_1, HW_2, implement HW_3

--- Lesson_1/Variable.java
+++ Lesson_1/Variable.java
@@ -1,12 +1,15 @@
 public class Variable {
 	public static void main (String[] args ) {
-		byte typeOne = -56;
-		short typeTwo = -11549;
-		int typeThree = 5;
-		long typeFour = 3123857;
-		float typeFive = 1853,18f;
-		double typeSix = 9,80665d;
-		char typeSeven = 'I';
-		boolean typeEight = true;
+		byte CPU = 3;
+		short model = 2100;
+		int minMemory = 1066;
+		long maxMemory = 1333;
+		float limit = 3.40f;
+		double limit2 = 3.40d;
+		char name = 'I';
+		boolean isStarted = true;
+		System.out.println(name + " i" + CPU + "-" + model + " " + limit + "Ghz" + " " + limit2 + " Ghz");
+		System.out.println("Частота оперативной памяти " + minMemory + "-" + maxMemory);
+		System.out.println(isStarted);
 	}
 }
