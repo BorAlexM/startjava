56ce72469b79ca0f8a51fbd718b121bd75e07f4d
Alexandr <boralexm90@gmail.com>
2019-10-21 00:20:35 +0300

    corrected HW_4

--- Lesson_1/Calculator.java
+++ Lesson_1/Calculator.java
@@ -2,28 +2,23 @@ public class Calculator {
     public static void main(String[] args) {
         int num1 = 10;
         int num2 = 5;
-        int power = 3;
-        char operation1 = '+';
-        char operation2 = '-';
-        char operation3 = '*';
-        char operation4 = '/';
-        char operation5 = '^';
-        char operation6 = '%';
-        
+        char operation1 = '0';
+
         if (operation1 == '+') {
             System.out.println(num1 + num2);
-        } else if (operation2 == '-') {
+        } else if (operation1 == '-') {
             System.out.println(num1 - num2);
-        } else if (operation3 == '*') {
+        } else if (operation1 == '*') {
             System.out.println(num1 * num2);
-        } else if (operation4 == '/') {
+        } else if (operation1 == '/') {
             System.out.println(num1 / num2);
-        } else if (operation5 == '^') {
-            for (int i = 0; i < power; i++) {
-                num1 *= 10;
+        } else if (operation1 == '^') {
+        int power = 1;
+            for (int i = 0; i < num2; i++) {
+                power *= num1;
             }
-            System.out.println(num1);
-        } else if (operation6 == '%') {
+            System.out.println(power);
+        } else if (operation1 == '%') {
             System.out.println(num1 % num2);
         }
     }
--- Lesson_1/MyFirstGame.java
+++ Lesson_1/MyFirstGame.java
@@ -1,24 +1,23 @@
 public class MyFirstGame {
     public static void main(String[] args) {
         int targetNumber = 10;
-        int[] gues = new int[]{2, 3, 5, 13, 55, 10};
+        int[] number = new int[]{2, 3, 5, 13, 55, 10};
 
-        System.out.println( "Я загадываю число от 0 до 100..." );
+        System.out.println("Я загадываю число от 0 до 100...");
 
-        for (int i = 0; i < gues.length; i++) {
-
-            if (gues[i] < targetNumber) {
-                System.out.println( "Введенное Вами число меньше того, что загадал компьютер" );
+        for (int i = 0; i < number.length; i++) {
+            if (number[i] < targetNumber) {
+                System.out.println("Введенное Вами число меньше того, что загадал компьютер");
             }
 
-            if (gues[i] > targetNumber) {
-                System.out.println( "Введенное Вами число больше того, что загадал компьютер" );
+            else if (number[i] > targetNumber) {
+                System.out.println("Введенное Вами число больше того, что загадал компьютер");
             }
 
-            if (gues[i] != targetNumber) {
-                System.out.println( "Попробуйте снова" );
+            if (number[i] != targetNumber) {
+                System.out.println("Попробуйте снова");
             }
         }
-        System.out.println( "Вы угадали!" );
+        System.out.println("Вы угадали!");
     }
 }
