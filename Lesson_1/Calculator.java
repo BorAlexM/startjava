e0af417336a60322c25da5ea057126990beb59e6
Alexandr <boralexm90@gmail.com>
2019-10-21 23:46:13 +0300

    corrected HW_4

--- Lesson_1/Calculator.java
+++ Lesson_1/Calculator.java
@@ -2,7 +2,7 @@ public class Calculator {
     public static void main(String[] args) {
         int num1 = 10;
         int num2 = 5;
-        char operation1 = '0';
+        char operation1 = '*';
 
         if (operation1 == '+') {
             System.out.println(num1 + num2);
@@ -13,7 +13,7 @@ public class Calculator {
         } else if (operation1 == '/') {
             System.out.println(num1 / num2);
         } else if (operation1 == '^') {
-        int power = 1;
+            int power = 1;
             for (int i = 0; i < num2; i++) {
                 power *= num1;
             }
--- Lesson_1/MyFirstGame.java
+++ Lesson_1/MyFirstGame.java
@@ -1,20 +1,18 @@
 public class MyFirstGame {
     public static void main(String[] args) {
         int targetNumber = 10;
-        int[] number = new int[]{2, 3, 5, 13, 55, 10};
+        int[] numbers = new int[]{2, 3, 5, 13, 55, 10};
 
         System.out.println("Я загадываю число от 0 до 100...");
 
         for (int i = 0; i < number.length; i++) {
-            if (number[i] < targetNumber) {
+            if (numbers[i] < targetNumber) {
                 System.out.println("Введенное Вами число меньше того, что загадал компьютер");
-            }
-
-            else if (number[i] > targetNumber) {
+            } else if (numbers[i] > targetNumber) {
                 System.out.println("Введенное Вами число больше того, что загадал компьютер");
             }
 
-            if (number[i] != targetNumber) {
+            if (numbers[i] != targetNumber) {
                 System.out.println("Попробуйте снова");
             }
         }
