8d86de796b1690bc7de82a39639814620e80dba5
Alexandr <boralexm90@gmail.com>
2019-10-26 17:36:32 +0300

    Lesson2, HW_1

---
+++ Lesson_2/Person.java
@@ -0,0 +1,36 @@
+public class Person {
+	
+	String sex = "Male";
+	String personName = "Rob";
+	float height = 1.72f;
+	float weigth = 81.6f;
+	int personAge = 36;
+
+
+/*
+
+Создайте в классе методы, описывающие поведение (что он может делать) человека: идти, сидеть, бежать, говорить, учить Java
+
+*/
+
+	void go() {
+		
+	}
+
+	void sit() {
+		
+	}
+
+	void run() {
+		
+	}
+
+	void talk() {
+		
+	}
+
+	boolean teachJava() {
+		System.out.println(personName + " учит Java");
+		return true;
+	}	
+}
