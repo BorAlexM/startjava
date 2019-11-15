145e1c1a85bc7eeebd890cb93bfb91a9e5f5a9fe
Alexandr <boralexm90@gmail.com>
2019-11-15 18:30:21 +0300

    Corrected GuessNumberTest

--- Lesson_2/GuessNumberTest.java
+++ Lesson_2/GuessNumberTest.java
@@ -3,18 +3,18 @@ import java.util.Scanner;
 public class GuessNumberTest {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        String play;
+        String playerAnswer;
         do {
-            System.out.println("Введите имя первого игрока");
+            System.out.print("Введите имя первого игрока");
             Player p1 = new Player(scan.nextLine());
-            System.out.println("Введите имя второго игрока");
+            System.out.print("Введите имя второго игрока");
             Player p2 = new Player(scan.nextLine());
             GuessNumber game = new GuessNumber(p1, p2);
             game.startGame();
             do {
-                System.out.println("Хотите продолжить? [да/нет]");
-                play = scan.nextLine();
-            } while (!"нет".equals(play) && !"да".equals(play));
-        } while (!"нет".equals(play));
+                System.out.print("Хотите продолжить? [да/нет]");
+                playerAnswer = scan.nextLine();
+            } while (!"нет".equals(playerAnswer) && !"да".equals(playerAnswer));
+        } while (!"нет".equals(playerAnswer));
     }
 }
